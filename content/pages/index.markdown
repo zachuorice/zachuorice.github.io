e988521dd6cc761324136319834253ede48abc4d
Zachary Richey <zr.public@gmail.com>
2016-07-01 23:50:56 -0400

    Barebones pelican site.

---
+++ content/pages/index.markdown
@@ -0,0 +1,13 @@
+Title: Hi! Who am I?
+Date: 2016-07-01 23:07
+Slug: hi
+Author: Zachary Richey
+Summary: Welcome, I am a programmer, amateur musician, college graduate, and many many more labels. However I don't really know what I am.
+
+Thank you for taking the time to read this. This is me:
+
+<img src="{filename}/images/me.jpg" style="width: 50%" />
+
+I'm currently Senior Web Developer at [PRONETS](http://pronetsweb.com). Which basically means PHP, CSS, Wordpress, and server administration. But you're not here for that, no, you're here for interesting, fulfilling, life-changing, incredible, AMAZING content!... -*Well that's not going to happen*- but perhaps you'll find something of interest on my site.
+
+Feel free to look around, there isn't much here yet. I hope to write about code, my experiences, music, music transcription, and probably more.
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Zachary Richey'
+SITENAME = 'Zachuorice'
+SITEURL = 'http://zachuorice.com'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/zachuorice'),
+          ('Facebook', 'https://www.facebook.com/profile.php?id=100009557919910'),
+          ('Youtube', 'https://www.youtube.com/c/ZachuoriceRich'),
+          ('Github', 'https://github.com/zachuorice/'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
