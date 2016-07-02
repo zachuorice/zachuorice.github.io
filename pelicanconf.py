12ee4b2571ec46fb2df6535e0ee3f012f6d60c1e
Zachary Richey <zr.public@gmail.com>
2016-07-02 17:41:51 -0400

    Zachuorice theme skeleton (copy of simple template).

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,4 +32,6 @@ SOCIAL = (('Twitter', 'https://twitter.com/zachuorice'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+THEME="zachuorice"
